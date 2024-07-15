a7e93667dc326da18c476594551520c0e02ace1a
Sajad Nowroozi <sajad.nowroozi5442@gmail.com>
2024-07-15 11:08:47 +0430

    Change background color whit Sajad😁

--- lib/my_home.dart
+++ lib/my_home.dart
@@ -1,11 +1,35 @@
-
-
-
-
 import 'package:flutter/material.dart';
 
-void main(){
-  Text t = Text("In the name of Allah", textDirection: TextDirection.ltr, style: TextStyle(fontSize: 50.0, color: Colors.blue),);
-  Center c = Center(child: t,);
+main() {
+  var c = Container(
+    width: 200,
+    height: 200,
+    color: Colors.deepPurple,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          '786',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+              fontSize: 50, color: Colors.blue, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          'This is Flutter',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(fontSize: 30, color: Colors.green),
+        ),
+        Text(
+          'flutter is a UI tookit developed by Google ',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.deepOrange,
+          ),
+        ),
+      ],
+    ),
+  );
+
   runApp(c);
 }
