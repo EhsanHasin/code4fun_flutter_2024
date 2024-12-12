cb424bc66a6759fb0a4c7067889e584e69133fd5
Ehsan Hasin <ehsanhasin@gmail.com>
2024-12-12 12:45:59 +0430

    Working on files, sharedPreferences and sqflite

--- lib/flutter persistant data storage/share_preferences/shared1.dart
+++ lib/flutter persistant data storage/share_preferences/shared1.dart
@@ -15,31 +15,40 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  TextEditingController controller = TextEditingController();
+  TextEditingController keyController = TextEditingController();
+  TextEditingController valueController = TextEditingController();
   var data = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Column(
-          children: [
-            TextField(
-              controller: controller,
-            ),
-            ElevatedButton(onPressed: () async{
-              final SharedPreferences prefs = await SharedPreferences.getInstance();
-              await prefs.setString("myData", controller.text);
-            }, child: Text("save")),
-            ElevatedButton(onPressed: () async{
-              final SharedPreferences prefs = await SharedPreferences.getInstance();
-              final String? action = prefs.getString('myData');
-              setState(() {
-                data = action ?? "empty | null";
-              });
-            }, child: Text("restore")),
-            Text(data)
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: keyController,
+                decoration: InputDecoration(hintText: "key"),
+              ),
+              TextField(
+                controller: valueController,
+                decoration: InputDecoration(hintText: "value"),
+              ),
+              ElevatedButton(onPressed: () async{
+                final SharedPreferences prefs = await SharedPreferences.getInstance();
+                await prefs.setString(keyController.text, valueController.text);
+              }, child: Text("save")),
+              ElevatedButton(onPressed: () async{
+                final SharedPreferences prefs = await SharedPreferences.getInstance();
+                final String? action = prefs.getString(keyController.text);
+                setState(() {
+                  data = action ?? "empty | null";
+                });
+              }, child: Text("restore")),
+              Text(data)
+            ],
+          ),
         ),
       ),
     );
